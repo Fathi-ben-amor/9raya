0d03d5c69c6e18ddc9f1510471b477f71c683082
fathi <fathibenamor2000@gmail.com>
2021-12-19 22:45:38 +0100

    kraya

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+    (function($) {
+    "use strict";
+    
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
